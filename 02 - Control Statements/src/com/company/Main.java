71e4e9c59a2add8adf6ce5f4da86d448e5e62d95
Elad Lavi <eladlavi@Elads-MacBook-Pro.local>
2016-07-06 19:32:31 +0300

    Control Statement

---
+++ 02 - Control Statements/src/com/company/Main.java
@@ -0,0 +1,74 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 8;
+        int y = 19;
+        /*
+        System.out.println("start");
+        if(x < 5) {
+            System.out.println("x is less than five");
+        }else if(x < 10){
+            System.out.println("x is between 5 and 10");
+        }else if(x < 20){
+            System.out.println("x is between 10 and 20");
+        }else{
+            System.out.println("x is greater than or equal to 20");
+        }
+        System.out.println("end");
+        */
+
+        /*
+        int y = x++;
+        System.out.println(y); //y is 8! if the line before was:
+        //int y = ++x then y was 9
+        */
+
+
+
+        while(x < 20){
+            System.out.println("x is less than 20");
+            x++;
+        }
+        System.out.println(x);
+
+
+        //System.out.println("1" + (1 + 1));
+
+
+        /*int i = 0;
+        while(i<10){
+            System.out.println("i is " + i);
+            i++;
+        }*/
+
+
+
+        for (int i = 0; i < x; i++) {
+            System.out.println("i is " + i);
+        }
+
+        int a = x;
+        int b = y;
+        if(x > y){
+            a = y;
+            b = x;
+        }
+        int z = myFunc(y);
+        System.out.println(z);
+    }
+
+    public static int myFunc(int x){
+        System.out.println("in myFunc " + x);
+        return x + 80;
+    }
+
+    public static int distance(int x, int y){
+        int d = x - y;
+        if(d < 0)
+            d *= -1;
+        return d;
+    }
+}
