c492a040d961d0a35b406991e7ceb7a15a227648
Elad Lavi <eladlavi@Elads-MacBook-Pro.local>
2016-07-24 21:21:31 +0300

    merge sort

--- Java - Basic Program/Lesson 08 - Arrays/src/com/company/Main.java
+++ Java - Basic Program/Lesson 08 - Arrays/src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {-1, 4, -1, 3, -3};
-        System.out.println(maxSubArray(nums));
+        int[] nums = {4, 7, 2, 5, 3, 6};
+        mergeSort(nums, 0, nums.length - 1);
+        printArray(nums);
     }
 
     public static void reverseOrder(int[] nums){
@@ -45,5 +46,97 @@ public class Main {
     }
 
 
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+        for (int j = low; j<high; j++) {
+            if(arr[j] <= pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+
+
+    public static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p-1);
+            quickSort(arr, p+1, high);
+        }
+    }
+
+
+    public static void printArray(int arr[]){
+
+        System.out.print("{");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        if(arr.length > 0)
+            System.out.print(arr[arr.length - 1]);
+        System.out.println("}");
+
+        /*
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i + (i==arr.length-1 ? "" : ","));
+        }
+        */
+    }
+
+
+    public static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int leftArray[] = new int[n1];
+        int rightArray[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            rightArray[i] = arr[middle + 1 + i];
+        }
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2){
+            if(leftArray[i] <= rightArray[j]){
+                arr[k] = leftArray[i];
+                i++;
+            }else{
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1){
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+
+    }
+
+    public static void mergeSort(int[] arr, int left, int right){
+        if(left < right){
+            int middle = (left+right)/2;
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+
+            merge(arr, left, middle, right);
+        }
+    }
 
 }
