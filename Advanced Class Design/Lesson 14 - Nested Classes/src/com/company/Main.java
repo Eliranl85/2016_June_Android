2f953a52eb52b6082204059ab122e0c18e45a14d
Elad Lavi <eladlavi@Elads-MacBook-Pro.local>
2016-08-11 08:40:05 +0300

    Lesson 14 - Nested Classes

---
+++ Advanced Class Design/Lesson 14 - Nested Classes/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        four types of nested classes:
+        - static nested class
+        - inner class
+        - local inner class
+        - anonymous class
+         */
+    }
+}
