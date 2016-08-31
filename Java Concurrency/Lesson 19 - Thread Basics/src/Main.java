890c0b384cad73577954c5b09233058e47e830e5
Elad Lavi <eladlavi@Elads-MacBook-Pro-2.local>
2016-08-31 18:58:34 +0300

    Threads

---
+++ Java Concurrency/Lesson 19 - Thread Basics/src/Main.java
@@ -0,0 +1,35 @@
+/**
+ * Created by eladlavi on 31/08/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("start");
+
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hi");
+                int result = longRunningTask();
+                System.out.println("end " + result);
+            }
+        });
+        t.start();
+
+
+
+        System.out.println("done");
+    }
+
+    public static int longRunningTask(){
+        int x = 0;
+        for (int i = 0; i < 2000000000; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(i%2==0)
+                    x++;
+            }
+        }
+        return x;
+    }
+
+}
